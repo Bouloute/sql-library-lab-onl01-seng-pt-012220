de590791f7f73ae29995cc92a54d150331f48bcc
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-18 21:15:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) FROM characters
     GROUP BY characters.species
-    ORDER BY COUNT(characters.species)
+    ORDER BY DESC(COUNT(characters.species))
     LIMIT 1"
 end
 
