8ce2ce6970ca2dd80c44df1f5d85b7b4e53f2a4f
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-18 20:39:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,5 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("This Series", 1, 1), ("That Series", 2, 2);
 
 INSERT INTO books (title) VALUES ("Philosopher Stone"), ("Prisoner of Azkaban"), ("Goblet of Fire"), ("Half Blood Prince"), ("Chamber of Secrets"), ("Order of Phenix");
+
+INSERT INTO characters (name) VALUES ("Harry"), ("Hermione"), ("Ron"), ("Malfoy"), ("Neville"), ("Dudley"), ("Voldemort"), ("Ginny");
