4afbbf9b418864dd9ff01072b7e2f19d925a4319
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-18 21:54:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,6 +29,7 @@ def select_series_title_with_most_human_characters
     JOIN authors ON series.author_id = authors.id
     JOIN characters ON characters.author_id = authors.id
     WHERE characters.species = \"human\"
+    GROUP BY series.title
     ORDER BY COUNT(*)"
 end
 
